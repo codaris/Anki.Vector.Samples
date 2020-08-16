7e66328398cfa2b3101b6ec23b0764a9db46d650
Wayne Venables <wayne@codaris.com>
2020-08-16 11:04:30 -0700

    Update to SDK 0.7.0

--- Tutorial_06_FaceImage/Program.cs
+++ Tutorial_06_FaceImage/Program.cs
@@ -5,6 +5,7 @@
 using System;
 using System.Reflection;
 using System.Runtime.InteropServices;
+using System.Threading;
 using System.Threading.Tasks;
 using Anki.Vector;
 using Anki.Vector.Types;
@@ -23,13 +24,12 @@ namespace Tutorial_06_FaceImage
         {
             // Get image
             using var resourceStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("Tutorial_06_FaceImage.cozmo_image.jpg");
-            using var image = Image.Load<Rgb24>(resourceStream);
+            using var image = SixLabors.ImageSharp.Image.Load<Rgb24>(resourceStream);
 
             // Create a new connection to the first configured Vector
             using var robot = await Robot.NewConnection();
 
-            // Data in Rgb24 format
-            var data = MemoryMarshal.AsBytes(image.GetPixelSpan()).ToArray();
+            var data = ConvertImageToByteArray(image);
 
             Console.WriteLine("Requesting control of Vector...");
             await robot.Control.RequestControl();
@@ -41,6 +41,21 @@ namespace Tutorial_06_FaceImage
             Console.WriteLine("Displaying image for 5 seconds...");
             await robot.Screen.DisplayImageRgb24(data, 5000);
             await Task.Delay(5000);
+        }
+
+        /// <summary>
+        /// Converts the image data to a byte array
+        /// </summary>
+        /// <param name="image">The image.</param>
+        /// <returns></returns>
+        /// <exception cref="NotSupportedException">Cannot extract pixel data from image.</exception>
+        private static byte[] ConvertImageToByteArray(Image<Rgb24> image)
+        {
+            if (image.TryGetSinglePixelSpan(out var pixelSpan))
+            {
+                return MemoryMarshal.AsBytes(pixelSpan).ToArray();
+            }
+            throw new NotSupportedException("Cannot extract pixel data from image.");
         }
     }
 }
